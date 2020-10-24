88cb983d6012a2905e772a9823612dc674359d55
Mattia Louis Vik Bradascio <28816406+bredamatt@users.noreply.github.com>
2020-10-24 23:37:20 +0100

    elongate comment

--- src/cholesky/CholeskyDecomposition.h
+++ src/cholesky/CholeskyDecomposition.h
@@ -3,5 +3,5 @@
 #include <vector>
 using namespace std;
 
-// Should take a matrix as input and decompose it
+// Should take a matrix as input and decompose it to its lower triangle matrix
 vector<vector<double>> CholeskyDecomposition(vector<vector<double>> matrix);
