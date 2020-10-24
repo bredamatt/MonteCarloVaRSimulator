02b9ba6e4f134e49a79fcfb690836593ed336c7a
Mattia Louis Vik Bradascio <28816406+bredamatt@users.noreply.github.com>
2020-10-24 20:13:17 +0100

    update definition based on implementation

--- src/headers/ReturnType.h
+++ src/headers/ReturnType.h
@@ -2,20 +2,10 @@
 
 /*----------------------------------------------------------------
 Definitions for returns:
-   1. Absolute = (Current - Investment) / Investment
-    
-        Absolute return example:
-            Investment = £8,000
-            Current value = £10,000
+    1. Absolute = (Current - Previous)
 
-        Absolute return = (10,000-8,000) / 8,000 = 25%
+    2. Relative = log(Current / Previous)
 
-    2. Relative = (Current - Previous)
-
-        Relative return example:
-            Y1 = 21.6%
-            Y2 = 15.3%
-
-        Relative return Y2 to Y1 = (15.3% - 21.6%) = -6.3%
+See TSHandler.cpp for details and implementation
 */
 enum class AbsOrRel { abs, rel };
