f81a1100ed02099fbcc03d7024df3c3c5885efdf
Mattia Louis Vik Bradascio <28816406+bredamatt@users.noreply.github.com>
2020-10-25 00:28:42 +0100

    add CholeskyDecomp test in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+#define M 3
+#define N 4
+
 int main() {
-    cout << "Hello there..." << endl;
+    cout << "Testing CholeskyDecomposition" << endl;
+
+    vector<vector<double>> matrix;
+    matrix.resize(M, vector<double>(N, 2));
+
+    cout << "Printing initial matrix" << endl;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << matrix[i][j] << ' ';
+        }
+    }
+
+    vector<vector<double>> lower = CholeskyDecomposition(matrix);
+
+    cout << "Printing lower triangle matrix" << endl;
+    for (int i = 0; i < lower.size(); i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << lower[i][j] << ' ';
+        }
+    }
+    return 0;
 }
