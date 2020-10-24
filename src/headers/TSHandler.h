9a14c910e86d67b1a69c2f49d02f23daf07215ba
Mattia Louis Vik Bradascio <28816406+bredamatt@users.noreply.github.com>
2020-10-24 19:02:58 +0100

    add privates

--- src/headers/TSHandler.h
+++ src/headers/TSHandler.h
@@ -7,7 +7,6 @@ using namespace std;
 class TSHandler
 {
     public:
-
         TSHandler(vector<vector<double>> riskFactorTimeSeries_,
                   vector<AbsOrRel> absOrRelReturnsVector_,
                   unsigned long daysBackUsed_ = 252);
@@ -18,6 +17,13 @@ class TSHandler
         double ComputeCovariance(vector<vector<double>> d, int i, int j);
         void CreateCovarianceMatrix(unsigned long startingDaysBack = 0);
         vector<vector<double>> GetCovarianceMatrix() const;
-
-        
-}
+        vector<vector<double>> GetPartsOfCovarianceMatrix(vector<unsigned long> diagonalIndices) const;
+    
+    private:
+        bool transformedToReturns;
+        vector<vector<double>> riskFactorTimeSeries;
+        vector<AbsOrRel> absOrRelReturnsVector;
+        vector<vector<double>> returns;
+        vector<vector<double>> covMatrix;
+        unsigned long daysBackUsed;
+};
