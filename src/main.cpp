b9d2cb94f05b01bc0f144598f116a31ea0ed73a8
Mattia Louis Vik Bradascio <28816406+bredamatt@users.noreply.github.com>
2020-10-25 10:43:34 +0000

    add ReadVSV and tsHandler in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "tshandler/TSHandler.cpp"
 #include "cholesky/CholeskyDecomposition.cpp"
+#include "parsers/ReadCSV.cpp"
 
 using namespace std;
 
@@ -8,25 +9,36 @@ using namespace std;
 #define N 4
 
 int main() {
-    cout << "Testing CholeskyDecomposition" << endl;
+    string file = "data/RiskFactorsHistory.csv";
+    vector<vector<double>> riskFactors = parse2DCsvFile(file);
+    vector<AbsOrRel> absOrRelVec;
 
+    TSHandler tsHandler(riskFactors, absOrRelVec);
+
+    vector<double> rets = tsHandler.GetMostRecentValues();
+
+    // tsHandler.CreateCovarianceMatrix(0);
+    // vector<vector<double>> covMatrix = tsHandler.GetCovarianceMatrix();
+    // vector<vector<double>> lowerMatrix = CholeskyDecomposition(covMatrix);
+
+    // Testing Cholesky Decomposition
+    cout << "Testing CholeskyDecomposition" << endl;
     vector<vector<double>> matrix;
     matrix.resize(M, vector<double>(N, 2));
-
     cout << "Printing initial matrix" << endl;
     for (int i = 0; i < matrix.size(); i++) {
         for (int j = 0; j <= i; j++) {
             cout << matrix[i][j] << ' ';
         }
     }
-
+    cout << endl;
     vector<vector<double>> lower = CholeskyDecomposition(matrix);
-
     cout << "Printing lower triangle matrix" << endl;
     for (int i = 0; i < lower.size(); i++) {
         for (int j = 0; j <= i; j++) {
             cout << lower[i][j] << ' ';
         }
     }
+    cout << endl;
     return 0;
 }
